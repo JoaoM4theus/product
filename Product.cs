cea4a5cf840785876cfb7ab358f22a89f1cd8a2b
JoaoM4theus <61093343+JoaoM4theus@users.noreply.github.com>
2020-03-11 18:29:37 -0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Product {
+    class Product {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public Product() {
+
+        }
+
+        public Product(string name, double price, int quantity) {
+            Name = name; Price = price; Quantity = quantity;
+        } 
+
+        public Product(string name, double price) {
+            Name = name; Price = price;
+        }
+
+        public double ValueInStock() {
+            return Price * Quantity;
+        }
+
+        public void AddProducts(int quantity) {
+            Quantity += quantity;
+        }
+
+        public void RemoveProducts(int quantity) {
+            Quantity -= quantity;
+        }
+
+        public override string ToString() {
+            return Name + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Quantity + " units. Total: $" + ValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Product {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantity in stock: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Product p = new Product(name, price, quantity);
+
+            Product p2 = new Product(name, price);
+
+            Product p3 = new Product {
+                Name = "TV",
+                Price = 500.00,
+                Quantity = 20
+            };
+
+            Console.WriteLine("Product Data: " + p);
+
+            Console.WriteLine();
+
+            Console.Write("Enter the product number to be added to stock: ");
+            int qti = int.Parse(Console.ReadLine());
+            p.AddProducts(qti);
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+
+            Console.WriteLine();
+            Console.Write("Enter the number of products to be removed from stock: ");
+            qti = int.Parse(Console.ReadLine());
+            p.RemoveProducts(qti);
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+
+        }
+    }
+}
